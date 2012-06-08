05aa74a6f457ce15249beb9edef45e679b133ccd
Antoine Kaufmann <toni@famkaufmann.info>
2012-06-08 11:50:53 +0200

    kgview: Erste Version des Clipboard-Importers fuer Diagnoselisten (Feature #84)
    Erlaubt es Diagnoselisten die als Aufzaehlungslisten in
    OpenOffice-Dokumenten stehen via Copy&Paste zu importieren.
    
    Das ganze ist aber nicht sehr Robust. Muss noch mit ein paar Dokumenten
    getestet werden. Wird wohl auch nicht fuer immer drin bleiben.

--- com.hilotec.elexis.kgview/src/com/hilotec/elexis/kgview/diagnoseliste/DiagnoselisteView.java
+++ com.hilotec.elexis.kgview/src/com/hilotec/elexis/kgview/diagnoseliste/DiagnoselisteView.java
@@ -1,19 +1,38 @@
 package com.hilotec.elexis.kgview.diagnoseliste;
 
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Stack;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.custom.TreeEditor;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.HTMLTransfer;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.part.ViewPart;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 
+import ch.elexis.Desk;
 import ch.elexis.actions.ElexisEvent;
 import ch.elexis.actions.ElexisEventDispatcher;
 import ch.elexis.actions.ElexisEventListener;
@@ -71,6 +90,324 @@ class MultilinePaintListener implements Listener {
 	}
 }
 
+
+/**
+ * Nicht-persistente Representation eines Diagnosebaums.
+ */
+class DNode {
+	public String text = "";
+	public ArrayList<DNode> children = new ArrayList<DNode>();
+
+	/**
+	 * Neues Kindelement erstellen.
+	 */
+	public DNode newChild() {
+		DNode dn = new DNode();
+		children.add(dn);
+		return dn;
+	}
+
+	/**
+	 * Text an diesen Knoten anhaengen
+	 */
+	public void append(String s) {
+		text += s;
+	}
+
+	/**
+	 * Text aufgeraeumt zurueckgeben
+	 */
+	public String getClean() {
+		return text.trim()
+					.replaceAll("[ \t]+", " ")
+					.replaceAll("\n+", "\n");
+	}
+
+	/**
+	 * Unter-Nodes in DiagnoselisteItem persistent abspeichern
+	 */
+	public void storeChildren(DiagnoselisteItem it) {
+		for (DNode c: children) {
+			DiagnoselisteItem ci = it.createChild();
+			ci.setText(c.getClean());
+			c.storeChildren(ci);
+		}
+	}
+}
+
+
+/**
+ * Dialog zum anzeigen eines Baums aus DNodes.
+ */
+class DLDialog extends TitleAreaDialog {
+	private DNode di;
+	private Tree tree;
+
+	public DLDialog(Shell parentShell, DNode di) {
+		super(parentShell);
+		this.di = di;
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		ScrolledComposite scroll = new ScrolledComposite(parent, SWT.BORDER | SWT.V_SCROLL);
+		Composite comp = new Composite(scroll, SWT.NONE);
+
+		scroll.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
+		scroll.setContent(comp);
+		comp.setLayout(new GridLayout());
+		comp.setLayoutData(SWTHelper.getFillGridData(1, true, 1, false));
+
+		tree = new Tree(comp, SWT.NONE);
+		tree.setLayoutData(SWTHelper.getFillGridData(1, true, 1, false));
+		final TreeEditor editor = new TreeEditor(tree);
+		editor.grabHorizontal = true;
+		editor.grabVertical = true;
+
+		MultilinePaintListener mlListener = new MultilinePaintListener();
+		tree.addListener(SWT.MeasureItem, mlListener);
+		tree.addListener(SWT.PaintItem, mlListener);
+		tree.addListener(SWT.EraseItem, mlListener);
+
+		addNodes(di, null);
+
+		comp.setSize(comp.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		return scroll;
+	}
+
+	private void addNodes(DNode dn, TreeItem parent) {
+		TreeItem ti;
+		for (DNode n: dn.children) {
+			if (parent == null)
+				ti = new TreeItem(tree, 0);
+			else
+				ti = new TreeItem(parent, 0);
+
+			ti.setText(n.getClean());
+			addNodes(n, ti);
+			ti.setExpanded(true);
+		}
+	}
+
+	@Override
+	public void create(){
+		super.create();
+		getShell().setText("Diagnosen Importieren");
+	}
+
+	@Override
+	protected void okPressed() {
+		close();
+	}
+}
+
+
+/**
+ * Parser um copy&paste-Daten aus OpenOffice zu uebernehmen. Sehr hacky.
+ */
+class DLParser {
+	/**
+	 * Versucht den String in eine DNode-Repraesentation zu parsen
+	 */
+	public DNode parse(String html) {
+		DNode dn = parseItems(html);
+		if (dn == null) {
+			dn = parseManualList(html);
+		}
+		System.out.println(dn);
+		if (dn != null) {
+			for (DNode c: dn.children) {
+				System.out.println(c.text);
+			}
+		}
+		return dn;
+	}
+
+	/**
+	 * Root Element fuer XML Dokument geparst aus code holen
+	 */
+	private Element parseXML(String code) {
+		try {
+			byte[] bytes = code.getBytes();
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+
+			dbFactory.setValidating(false);
+			dbFactory.setNamespaceAware(true);
+			dbFactory.setIgnoringComments(false);
+			dbFactory.setIgnoringElementContentWhitespace(false);
+			dbFactory.setExpandEntityReferences(false);
+
+			DocumentBuilder dBuilder;
+			dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(new ByteArrayInputStream(bytes));
+			return doc.getDocumentElement();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * HTML UL-Aufzaehlungsliste parsen
+	 */
+	private  DNode parseItems(String html) {
+		System.out.println("{{" + html + "}}");
+
+		Element e = parseXML(html);
+		if (e == null) return null;
+
+		DNode dn = new DNode();
+		parseUL(e, dn);
+		return dn;
+	}
+
+	/**
+	 * Konkretes UL-Element parsen, indirekt rekursiv. Dabei wird dn benutzt um
+	 * die entstehenden Kindknoten einzutragen.
+	 */
+	private void parseUL(Element e, DNode dn) {
+		if (!e.getNodeName().equals("ul")) return;
+		NodeList nl = e.getChildNodes();
+		for (int i = 0; i < nl.getLength(); i++) {
+			Node n = nl.item(i);
+			if (n instanceof Element && n.getNodeName().equals("li")) {
+				Element li = (Element) n;
+				li.normalize();
+				parseLI(li, dn.newChild());
+			}
+		}
+	}
+
+	/**
+	 * Einzelnes LI-Element einer UL-Liste parsen, rekursiv. Der Text und
+	 * allfaellige Kindknoten werden in dn eingetragen.
+	 */
+	private void parseLI(Element li, DNode dn) {
+		NodeList nl = li.getChildNodes();
+
+		for (int i = 0; i < nl.getLength(); i++) {
+			Node n = nl.item(i);
+
+			if (n instanceof Element) {
+				Element e = (Element) n;
+				e.normalize();
+
+				if (e.getNodeName().equals("ul")) {
+					parseUL(e, dn);
+				} else if (e.getNodeName().equals("br")) {
+					dn.append("\n");
+				} else if (e.getNodeName().equals("p")) {
+					dn.append("\n");
+					parseLI(e, dn);
+					dn.append("\n");
+				} else {
+					parseLI(e, dn);
+				}
+			} else if (n instanceof Text) {
+				Text t = (Text) n;
+				dn.append(t.getTextContent().trim());
+			}
+		}
+	}
+
+	/**
+	 * Parsen der manuell erstellten Aufzaehlungslisten (mit - und tabulatoren)
+	 * Gar nicht allgemein.
+	 */
+	private DNode parseManualList(String html) {
+		try {
+			// Versuchen html in gueltiges XML umzuwandeln
+			String enc = "utf-8";
+			if(System.getProperty("os.name").startsWith("Windows"))
+				enc = "iso-8859-1";
+			html = "<?xml version=\"1.0\" encoding=\""+enc+"\"?>" +
+					"<root>\n" + html + "\n</root>";
+			html = html.replaceAll("<br>", "<br/>");
+			html = html.replaceAll("<BR>", "<br/>");
+			html = html.replaceAll("ALIGN=JUSTIFY", "");
+			html = html.replaceAll("&shy;", "-");
+
+			System.out.println("Cleaned xml:\n{{" + html + "}}");
+
+			// Parsen
+			Element root = parseXML(html);
+			NodeList nl = root.getChildNodes();
+
+			Stack<DNode> stack = new Stack<DNode>();
+			stack.push(new DNode());
+			for (int i = 0; i < nl.getLength(); i++) {
+				Node n = nl.item(i);
+				if (!(n instanceof Element)) continue;
+				Element e = (Element) n;
+
+				// Wir wollen nur P-elemente
+				String txt = e.getTextContent();
+				if (!e.getNodeName().equalsIgnoreCase("p") ||
+					!txt.startsWith("-"))
+				{
+					throw new Exception("Nicht p-Element");
+				}
+
+				// Pruefen ob das class-Attribut mit list-n- anfaengt
+				String c = e.getAttribute("class");
+				if (c.isEmpty()) c = e.getAttribute("CLASS");
+				c = c.toLowerCase();
+				if (!c.matches("liste?-[0-9]-.*")) {
+					throw new Exception("p-Element ohne passende class");
+				}
+
+				// level Zahl aus class parsen
+				int lvl = Integer.parseInt(
+						c.replaceAll("^liste?-", "").substring(0, 1));
+				while (stack.size() > lvl) stack.pop();
+				if (stack.size() < lvl) {
+					throw new Exception("Stack underflow beim parsen");
+				}
+
+				// Knoten erstellen
+				DNode dn = stack.peek().newChild();
+				parseManualP(dn, e);
+				// Text etwas aufraeumen
+				dn.text = dn.text.substring(1).replaceAll("[ \t\n\r]+", " ");
+				dn.text = dn.text.trim();
+				stack.push(dn);
+			}
+			while (stack.size() > 1) stack.pop();
+			return stack.pop();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Text aus P-Element in manuell erstellter Liste parsen
+	 * @throws Exception 
+	 */
+	private void parseManualP(DNode dn, Element e) throws Exception {
+		e.normalize();
+		NodeList nl = e.getChildNodes();
+		for (int i = 0; i < nl.getLength(); i++) {
+			Node n = nl.item(i);
+			if (n instanceof Text) {
+				dn.append(n.getNodeValue());
+			} else if (n instanceof Element &&
+					n.getNodeName().equalsIgnoreCase("br"))
+			{
+				dn.append("\n");
+			} else {
+				throw new Exception("Ungueltigen Node angetroffen: {" +
+						n.getNodeName()+"}");
+			}
+		}
+	}
+}
+
+
+
+/**
+ * View um die Diagnoseliste anzuzeigen und zu bearbeiten
+ */
 public class DiagnoselisteView extends ViewPart implements ElexisEventListener {
 	public static final String ID = "com.hilotec.elexis.kgview.DiagnoselisteView";
 	private Tree tree;
@@ -81,6 +418,7 @@ public class DiagnoselisteView extends ViewPart implements ElexisEventListener {
 	Action actDel;
 	Action actMoveUp;
 	Action actMoveDown;
+	Action actImportCB;
 	
 	private void setupTI(TreeItem ti, DiagnoselisteItem di) {
 		ti.setText(di.getText());
@@ -120,6 +458,7 @@ public class DiagnoselisteView extends ViewPart implements ElexisEventListener {
 		actDel.setEnabled(en);
 		actMoveUp.setEnabled(en);
 		actMoveDown.setEnabled(en);
+		actImportCB.setEnabled(en);
 		
 		if (pat == null) return;
 		
@@ -168,6 +507,37 @@ public class DiagnoselisteView extends ViewPart implements ElexisEventListener {
 		tree.addListener(SWT.PaintItem, mlListener);
 		tree.addListener(SWT.EraseItem, mlListener);
 		
+		actImportCB = new Action("Importieren") {
+			{
+				setImageDescriptor(Desk.getImageDescriptor(Desk.IMG_IMPORT));
+				setToolTipText("Aus Zwischenablage importieren");
+				setDescription("Sollen die Diagnosen wie unten angegeben "+
+						"importiert werden?");
+			}
+
+			@Override
+			public void run() {
+				// Daten aus Zwischenablage holen
+				Clipboard cb = new Clipboard(Desk.getDisplay());
+				String s = (String) cb.getContents(HTMLTransfer.getInstance());
+				if (s == null) return;
+
+				// Wurzel des Diagnosebaums holen
+				Patient p = ElexisEventDispatcher.getSelectedPatient();
+				DiagnoselisteItem root = DiagnoselisteItem.getRoot(p);
+
+				// Daten parsen
+				DNode dn = new DLParser().parse(s);
+				if (dn == null) return;
+
+				DLDialog di = new DLDialog(Desk.getTopShell(), dn);
+				if (di.open() == DLDialog.OK) {
+					dn.storeChildren(root);
+					updateTree(p);
+				}
+			}
+		};
+
 		actEdit = new Action("Bearbeiten") {
 			@Override
 			public void run() {
@@ -270,7 +640,7 @@ public class DiagnoselisteView extends ViewPart implements ElexisEventListener {
 		
 		// Menus oben rechts in der View
 		ViewMenus menus = new ViewMenus(getViewSite());
-		menus.createToolbar(actAdd, actMoveUp, actMoveDown);
+		menus.createToolbar(actAdd, actMoveUp, actMoveDown, actImportCB);
 		
 		menus.createControlContextMenu(tree, actAddChild, actDel);
 		
