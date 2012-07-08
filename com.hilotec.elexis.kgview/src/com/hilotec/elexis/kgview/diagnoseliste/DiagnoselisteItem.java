097a892a8e3a21e079a8a2da16f008cfccd6eb01
Antoine Kaufmann <toni@famkaufmann.info>
2012-07-08 15:43:04 +0200

    kgview: DiagnoselisteItem-Tabelle mit korrekter Versionsnummer erstellen
    Beim anlegen der Tabelle wird bisher die Versionsnummer 1 statt 2
    benutzn, was danach zu Exceptions fuehrt da das Upgrade natuerlich nicht
    klappt.

--- com.hilotec.elexis.kgview/src/com/hilotec/elexis/kgview/diagnoseliste/DiagnoselisteItem.java
+++ com.hilotec.elexis.kgview/src/com/hilotec/elexis/kgview/diagnoseliste/DiagnoselisteItem.java
@@ -9,7 +9,7 @@ import ch.rgw.tools.StringTool;
 import ch.rgw.tools.TimeTool;
 
 public class DiagnoselisteItem extends PersistentObject {
-	public static final String VERSION = "1";
+	public static final String VERSION = "2";
 	public static final String PLUGIN_ID = "com.hilotec.elexis.kgview";
 	
 	private static final String TABLENAME = "COM_HILOTEC_ELEXIS_KGVIEW_DIAGNOSE";
